9c631436874d2b69d4fa081eaf1c96eb1f2388b0
João Roberto Soares <dnreikronos2@gmail.com>
2024-07-24 23:04:42 -0300

    Created the animatio for the modal

--- js/index.js
+++ js/index.js
@@ -3,6 +3,50 @@ let modal = document.getElementById("myModal");
 let myButton = document.getElementById("myBtn");
 let span = document.getElementsByClassName("close")[0];
 
+const choises = ["rock" , "paper" , "scissor"];
+const playerScoreDisplay = document.getElementById("playerScore");
+const computerScoreDisplay = document.getElementById("computerScore");
+const resultDisplay = document.getElementById("result");
+let playerScore = 0;
+let computerScore = 0;
+
+ function playGame(playerChoise) {
+    const computerChoise = choises[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if(playerChoise === computerChoise) {
+        result = "IT's a TIE!"
+    }
+    else {
+        switch(playerChoise){
+            case "rock":
+                result = (computerChoise === "scissor") ? "YOU WIN!" : "YOU LOSE!";
+                 break;
+            case "paper":
+                result = (computerChoise === "rock") ? "YOU WIN!" : "YOU LOSE!";
+            case "scissor":
+                result = (computerChoise === "paper") ? "YOU WIN!" : "YOU LOSE!";
+        }
+    }
+
+    resultDisplay.textContent = result;
+
+    switch(result){
+        case "YOU WIN!":
+            playerScore++;
+            playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
+            break;
+        case "YOU LOSE!":
+            computerScore++;
+            computerScoreDisplay.textContent = `Computer Score: ${computerScore}` ;
+            break;
+    }
+
+
+}
+
+
+
 
 myButton.onclick = function() {
     modal.style.display = "block";
